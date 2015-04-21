943215b12ab626fdf5c26f5e8c2850a0ca13d009
Ross Chapman <ross_chapman@me.com>
2015-04-21 12:04:22 -0400

    Imported user code snippets

---
+++ popupalertviewcontroller.m
@@ -0,0 +1,13 @@
+// PopupAlertViewController
+// 
+//
+// IDECodeSnippetCompletionScopes: [CodeBlock]
+// IDECodeSnippetIdentifier: 3F6863DF-1D6A-49B0-9E0F-42DEA752F879
+// IDECodeSnippetLanguage: Xcode.SourceCodeLanguage.Objective-C
+// IDECodeSnippetUserSnippet: 1
+// IDECodeSnippetVersion: 0
+PopupAlertViewController *alertVC = [PopupAlertViewController popupWithType:<# Type #>
+                                                                   delegate:<# delegate #>
+                                                                    message:<# message #>];
+            
+[alertVC showWithVC:<# viewController #>];
